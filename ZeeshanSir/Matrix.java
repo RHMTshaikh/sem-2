64d13dcf08b08df3f97bdfe7b1134948d7376947
RHMTshaikh <142720402+RHMTshaikh@users.noreply.github.com>
2024-01-23 14:58:51 +0530

    Update Matrix.java

--- ZeeshanSir/Matrix.java
+++ ZeeshanSir/Matrix.java
@@ -15,6 +15,50 @@ public class Matrix{
             }
         }
     }
+    Matrix(int r, int c){
+        rows=r;
+        columns = c;
+        Random rdm = new Random();
+        matrix = new int [rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = rdm.nextInt(9)+1;
+            }
+        }
+    }
+
+    void transpose(){
+        for (int i = 0; i < rows-1; i++) {
+            for (int j = i+1; j < columns; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    Matrix multiply(Matrix m1){
+        int[][] matrix1 = m1.matrix;
+        int m1_row = m1.rows;
+        int m1_col = m1.columns;
+
+        Matrix product =  new Matrix(rows,m1_col);
+
+        if (columns == m1_row){
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < m1_col; j++) {
+                    for (int k =0; k < columns; k++){
+                        product.matrix[i][j] += matrix[i][k] * matrix1[k][j];
+                    }
+                }
+            }
+        }
+        else{
+            System.out.println("Product not Possible! \nInvalid Dimensions");
+        }
+        return product;
+    }
+    
+    
     void display(){
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
@@ -25,6 +69,13 @@ public class Matrix{
     }
     public static void main(String[] args) {
         Matrix m = new Matrix(4);
+        Matrix m1 = new Matrix(4);
+
+        System.out.println("matrix1");
         m.display();
+        System.out.println("matrix2");
+        m1.display();
+        System.out.println("Multiplication of matrices.");
+        m.multiply(m1).display();
     }
 }
